eccc9346639d2fc6473b3f370d46a51708d6721f
Tric333 <34879209+Tric333@users.noreply.github.com>
2018-03-31 16:14:04 +0800

    Update spider_main.py

--- spider_main.py
+++ spider_main.py
@@ -11,13 +11,19 @@ class SpiderMain(object):
         self.outputer=html_outputer.HtmlOutputer()#输出获取到的内容
 
     #获取待爬取城市链接
-    def CityUrl_Crwa(self,root_url):
+    def District_Crwa(self,root_url):
         html_cont=self.downloader.download(root_url)
-        cityurls_list=self.parser.cityurlparser(html_cont)
-        return cityurls_list
+        district_urls=self.parser.district(html_cont)
+        return district_urls
+
+    def Business_District_Crwa(self,root_url):
+        html_cont=self.downloader.download(root_url)
+        print('开始解析 {} '.format(root_url))
+        business_district_urls = self.parser.business_district_parser(html_cont)
+        return business_district_urls
 
 #爬虫主体程序
-    def crwa(self,city_url):
+    def crwa(self,district,business,city_url):
         count=0
         self.urls.add_new_url(city_url)#将根链接首先放入page页urllist
         while self.urls.has_new_url():
@@ -27,7 +33,7 @@ class SpiderMain(object):
                 html_cont=self.downloader.download(new_url)#下载页面内容
                 new_urls, new_data ,house_city= self.parser.parse(new_url, html_cont)#解析页面内容
                 self.urls.add_new_urls(new_urls)
-                self.outputer.output_excel(new_data,house_city)#写入excel
+                self.outputer.output_excel(district,business,new_data,house_city)#写入excel
                 #self.outputer.output_mysql(new_data,house_city)#写入mysql
                 print("第",count,"个网页【",new_url,"】输出成功--------------")
             except Exception as e:
@@ -45,6 +51,12 @@ class SpiderMain(object):
 if __name__=="__main__":
     obj_spider=SpiderMain()
     root_url="http://esf.xian.fang.com/"
-    city_urls=obj_spider.CityUrl_Crwa(root_url)
-    for city_url in city_urls:
-        obj_spider.crwa(city_url)
+    district_urls=obj_spider.District_Crwa(root_url)
+
+    for district, url in district_urls.items() :
+        district_urls[district] = obj_spider.Business_District_Crwa(url)
+        print(district_urls[district])
+
+    for district,business_dict in district_urls.items():
+        for business,url in business_dict.items():
+            obj_spider.crwa(district,business,url)
